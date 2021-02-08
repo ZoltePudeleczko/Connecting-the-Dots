09b7d80d39ea2f51dc4aabc81e7a8dcf73c9c369
ZoltePudeleczko <szzborowski@gmail.com>
2021-02-08 17:58:34 +0100

    Add placeholder main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Button startGameButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = startGameButton.GetComponent<Button>();
+        btn.onClick.AddListener(NewGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("GameScreen");
+    }
+}
